0fa42e5863a7a9e26ec3b07d022ea78c9300d75e
alewand <01187246@pw.edu.pl>
2025-11-13 15:30:09 +0100

    Created pokemon loading methods

---
+++ constants.py
@@ -0,0 +1,15 @@
+POKEMON_DATA_SET_PATH = "data/pokemon.csv"
+COMPLETE_POKEMON_DATA_SET_PATH = "data/complete_pokemon.csv"
+
+EXCLUDED_PARAMETERS = ["abilities",
+                       "base_egg_steps",
+                       "base_happiness",
+                       "base_total",
+                       "capture_rate",
+                       "classfication",
+                       "experience_growth",
+                       "height_m",
+                       "japanese_name",
+                       "percentage_male",
+                       "weight_kg",
+                       "generation"]
---
+++ data_set.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import constants as const
+
+
+def get_final_evolution_pokedex_numbers(
+        data_set_path: str = const.COMPLETE_POKEMON_DATA_SET_PATH
+        ) -> list[str]:
+    data = pd.read_csv(data_set_path)
+    final_evolutions = data[data["Final Evolution"] == 1.0]
+    return final_evolutions["Number"].astype(str).tolist()
+
+
+def get_pokemon(
+        data_set_path: str = const.POKEMON_DATA_SET_PATH,
+        include_legendary: bool = False,
+        include_only_final_evolutions: bool = True
+        ) -> pd.DataFrame:
+    data = pd.read_csv(data_set_path)
+
+    for parameter in const.EXCLUDED_PARAMETERS:
+        if parameter in data.columns:
+            data = data.drop(columns=[parameter])
+
+    if not include_legendary:
+        if "is_legendary" in data.columns:
+            data = data[data["is_legendary"] == 0]
+
+    if include_only_final_evolutions:
+        final_evolution_pokedex_numbers = get_final_evolution_pokedex_numbers()
+        data = (data[data["pokedex_number"].astype(str)
+                     .isin(final_evolution_pokedex_numbers)])
+    return data
