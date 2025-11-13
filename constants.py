28079d40fa7a60621ad496b8a19b80ee5cba743c
alewand <01187246@pw.edu.pl>
2025-11-13 19:10:33 +0100

    Created pokemon teams generating methods

--- constants.py
+++ constants.py
@@ -1,6 +1,15 @@
 POKEMON_DATA_SET_PATH = "data/pokemon.csv"
 COMPLETE_POKEMON_DATA_SET_PATH = "data/complete_pokemon.csv"
 
+TEAM_SIZE = 6
+POKEMON_TO_REPLACE_AMOUNT = 3
+
+FIRST_TYPE_COL = "type1"
+SECOND_TYPE_COL = "type2"
+ID_COL = "pokedex_number"
+NAME_COL = "name"
+IS_LEGENDARY_COL = "is_legendary"
+
 EXCLUDED_PARAMETERS = ["abilities",
                        "base_egg_steps",
                        "base_happiness",
@@ -13,3 +22,6 @@ EXCLUDED_PARAMETERS = ["abilities",
                        "percentage_male",
                        "weight_kg",
                        "generation"]
+
+NUMBER_COL = "Number"
+FINAL_EVOLUTION_COL = "Final Evolution"
--- data_set.py
+++ data_set.py
@@ -1,34 +1,42 @@
 import pandas as pd
-import constants as const
+
+from constants import (
+    COMPLETE_POKEMON_DATA_SET_PATH,
+    EXCLUDED_PARAMETERS,
+    FINAL_EVOLUTION_COL,
+    IS_LEGENDARY_COL,
+    POKEMON_DATA_SET_PATH,
+    NUMBER_COL
+)
 
 
-def get_final_evolutions_pokedex_numbers(
-        data_set_path: str = const.COMPLETE_POKEMON_DATA_SET_PATH
+def get_final_evolutions_ids(
+        data_set_path: str = COMPLETE_POKEMON_DATA_SET_PATH
         ) -> list[str]:
     data = pd.read_csv(data_set_path)
-    final_evolutions = data[data["Final Evolution"] == 1.0]
+    final_evolutions = data[data[FINAL_EVOLUTION_COL] == 1.0]
 
-    return final_evolutions["Number"].astype(str).tolist()
+    return final_evolutions[NUMBER_COL].astype(str).tolist()
 
 
-def get_pokemon_data(
-        data_set_path: str = const.POKEMON_DATA_SET_PATH,
+def get_pokemons_data(
+        data_set_path: str = POKEMON_DATA_SET_PATH,
         include_legendary: bool = False,
         include_only_final_evolutions: bool = True
         ) -> pd.DataFrame:
     data = pd.read_csv(data_set_path)
 
-    for parameter in const.EXCLUDED_PARAMETERS:
-        if parameter in data.columns:
-            data = data.drop(columns=[parameter])
-
     if not include_legendary:
-        if "is_legendary" in data.columns:
-            data = data[data["is_legendary"] == 0]
+        if IS_LEGENDARY_COL in data.columns:
+            data = data[data[IS_LEGENDARY_COL] == 0]
 
     if include_only_final_evolutions:
-        pokedex_numbers = get_final_evolutions_pokedex_numbers()
-        data = (data[data["pokedex_number"].astype(str)
-                     .isin(pokedex_numbers)])
+        final_evolutions_ids = get_final_evolutions_ids()
+        data = (data[data[NUMBER_COL].astype(str)
+                     .isin(final_evolutions_ids)])
+
+    for parameter in EXCLUDED_PARAMETERS:
+        if parameter in data.columns:
+            data = data.drop(columns=[parameter])
 
     return data
